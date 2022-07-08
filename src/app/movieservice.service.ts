ee31d7da5d65feb0f29457b339ee9c9286d9e6fb
Vishal Choudhary <vishal200712@gmail.com>
2022-07-08 15:34:30 +0530

    Implement json-server

--- src/app/movieservice.service.ts
+++ src/app/movieservice.service.ts
@@ -1,11 +1,24 @@
+import { Movie } from './movie';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MovieserviceService {
+  url: string = 'https://localhost:3000/movies';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-type': 'application/json',
+    }),
+  };
+
   Display(name: string): string {
     return 'Hello ' + name;
   }
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+  getmovies(): Observable<Movie[]> {
+    return this.httpClient.get<Movie[]>(this.url);
+  }
 }
